a09f0cbe616121ea76bf3e9dd21a896834bf4a25
oleg <reekee94@gmail.com>
2019-11-05 12:45:58 +0200

    trird commit

--- Practice1/6.js
+++ Practice1/6.js
@@ -1,3 +1,9 @@
+/* За допомогою функції prompt() напишіть скріпт, який послідовно запитує в користувача логін, емейл та пароль,
+і виводить на екран повідомлення із введеними даними.
+Наприклад “Dear User, your email is usermale@gmail.com, your password is qwerty”. */
+
+
+
 var userName = prompt('Enter your login');
 var userMail = prompt('Enter your email');
 var userPassowd = prompt("Enter your password");
