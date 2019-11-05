20779285fd9bb6493753abb64f4c252876de7707
oleg <reekee94@gmail.com>
2019-11-05 12:17:23 +0200

    second comit

--- Practice1/6.js
+++ Practice1/6.js
@@ -2,4 +2,4 @@ var userName = prompt('Enter your login');
 var userMail = prompt('Enter your email');
 var userPassowd = prompt("Enter your password");
 
-alert('Dear '+ userName + ', your email is '+ userMail + ', your password is '+ userPassowd)
+alert('Dear '+ userName + ', your email is '+ userMail + ', your password is '+ userPassowd);
