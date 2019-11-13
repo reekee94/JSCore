727b0b5f858fb846cf65202cf6a56300be2dcc91
oleg <reekee94@gmail.com>
2019-11-13 14:52:03 +0200

    homework 3 reload

--- Practice3/4.js
+++ Practice3/4.js
@@ -1,14 +1,15 @@
-let x = (+prompt("First digit:"));
-let y = (+prompt("Second digit:"));
+let secondDigit = (+prompt("First digit:"));
+let firstDigit = (+prompt("Second digit:"));
 
 
-while(!Number.isInteger(x) || !Number.isInteger(y)) {
-    x = (+prompt("First digit:"));
-    y = (+prompt("Second digit:"));
+while(!Number.isInteger(secondDigit) || !Number.isInteger(firstDigit)) {
+    secondDigit = (+prompt("First digit:"));
+    firstDigit = (+prompt("Second digit:"));
 }
 function raiseToDegree(a,b) {
-    return Math.pow(a, b);
+    return a**b
+    // return Math.pow(a, b);
 }
 
 
-console.log(raiseToDegree(x, y));
+console.log(raiseToDegree(secondDigit, firstDigit));
